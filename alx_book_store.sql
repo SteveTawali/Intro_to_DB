bfb34fb2e0650a9b2b3f7bc8a7c4104e89e47079
Steven Gulu <stevenkatawali@gmail.com>
2024-10-20 23:24:54 +0300

    Added Authors

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
